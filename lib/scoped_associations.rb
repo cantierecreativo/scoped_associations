20368ae3f88aff8246d1129c8b5be75b4e9cae28
David Librera <davidlibrera@gmail.com>
2016-01-27 19:16:24 +0100

    removed activerecord3 support

--- lib/scoped_associations.rb
+++ lib/scoped_associations.rb
@@ -1,10 +1,4 @@
 if defined?(ActiveRecord::Base)
-  if ActiveRecord::VERSION::MAJOR == 4
-    require 'scoped_associations/activerecord4/has_many'
-    require 'scoped_associations/activerecord4/has_one'
-  elsif ActiveRecord::VERSION::MAJOR == 3
-    require 'scoped_associations/activerecord3/has_many'
-    require 'scoped_associations/activerecord3/has_one'
-  end
+  require 'scoped_associations/activerecord4/has_many'
+  require 'scoped_associations/activerecord4/has_one'
 end
-
--- lib/scoped_associations/version.rb
+++ lib/scoped_associations/version.rb
@@ -1,3 +1,3 @@
 module ScopedAssociations
-  VERSION = "0.1.0"
+  VERSION = "0.1.1"
 end
